41b88d3a8d73f7a30582dbbe4690a2b43fbbef77
巴拉巴拉巴拉巴 <noahmiller@qq.com>
2025-11-18 17:54:01 +0800

    创建项目

---
+++ tessa-api/src/main/java/com/metasync/tessa/TessaApiApplication.java
@@ -0,0 +1,13 @@
+package com.metasync.tessa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TessaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TessaApiApplication.class, args);
+    }
+
+}
---
+++ tessa-api/src/test/java/com/metasync/tessa/TessaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metasync.tessa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TessaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
